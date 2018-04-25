30b2c28d76fe5296f077114d15f6c92f1417ac1b
ErkHal <erkkihalinen@gmail.com>
2018-04-25 13:57:07 +0300

    Added model for RecyclingSpot

---
+++ RecycleWhere/Models/RecyclingSpot+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  RecyclingSpot+CoreDataClass.swift
+//  RecycleWhere
+//
+//  Created by iosdev on 25.04.18.
+//  Copyright © 2018 Staham Nguyen. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class RecyclingSpot: NSManagedObject {
+
+}
---
+++ RecycleWhere/Models/RecyclingSpot+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  RecyclingSpot+CoreDataProperties.swift
+//  RecycleWhere
+//
+//  Created by iosdev on 25.04.18.
+//  Copyright © 2018 Staham Nguyen. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension RecyclingSpot {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RecyclingSpot> {
+        return NSFetchRequest<RecyclingSpot>(entityName: "RecyclingSpot")
+    }
+
+    @NSManaged public var lat: String?
+    @NSManaged public var lng: String?
+    @NSManaged public var material_id: String?
+    @NSManaged public var name: String?
+
+}
